79d512c78ec64d25239f5bfa83b310271740284d
Blackdow <150688385+JuliusCoding@users.noreply.github.com>
2024-09-10 17:04:15 +0200

    Add files via upload

---
+++ julius_xmenu/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'Blackdow'
+version '1.0.0'
+description 'Blackdow`s X-Menu'
+
+ui_page "html/index.html"
+
+files {
+	"html/index.html",
+	"html/imgs/*.png",
+	"html/imgs/function-icons/*.png",
+	
+
+}
+
+client_scripts {
+	"client.lua",
+}
+
+server_scripts {
+	"server.lua",
+}
